3f620cf23ae5b50fe03f9392ce3ef68f9662ce2d
ddammage <ddammmage@yandex.ru>
2022-08-22 15:05:46 +0300

    17

--- src/middlewares/bloggerId.ts
+++ src/middlewares/bloggerId.ts
@@ -1,3 +1,6 @@
 import {body} from "express-validator";
 
-export const bloggerIdValidation = body('bloggerId').isNumeric()
+// export const bloggerIdValidation = body('bloggerId').isNumeric()
+
+const bloggerIdBodyRegExp2 = /^\d+$/i
+export const bloggerIdValidation = body('bloggerId').matches(bloggerIdBodyRegExp2).withMessage('body.bloggerId must be Int')

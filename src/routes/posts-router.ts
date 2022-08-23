f9b9d513dd92b475066841f7b59e8f2c44fdc0ce
ddammage <ddammmage@yandex.ru>
2022-08-23 10:44:25 +0300

    18

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -41,14 +41,16 @@ postsRouter.post('/',
     (req: Request, res: Response) => {
         // if (!post) return res.status(400).send({errorsMessages: [{ message: 'Invalid bloggerId', field: "bloggerId" }] })
         const newPost = postsRepository.createPost(req.body.title, req.body.shortDescription, req.body.content, req.body.bloggerId)
-        if (!newPost) return res.status(400).send({
-            "errorsMessages": [
-                {
-                    "message": "Invalid value2",
-                    "field": "bloggerId"
-                }
-            ]
-        })
+        if (newPost === false) return res.status(400).send({errorsMessages: [{ message: 'Invalid bloggerId', field: "bloggerId" }] })
+        // if (!newPost) return res.status(400).send({errorsMessages: [{ message: 'Invalid bloggerId', field: "bloggerId" }] })
+        // if (!newPost) return res.status(400).send({
+        //     "errorsMessages": [
+        //         {
+        //             "message": 'Invalid bloggerId',
+        //             "field": "bloggerId"
+        //         }
+        //     ]
+        // })
        res.status(201).send(newPost)
     })
 postsRouter.delete('/:id',

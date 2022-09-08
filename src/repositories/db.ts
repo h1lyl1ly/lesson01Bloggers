80a89eaf3608e185de6553690c3e9048abc8ca6f
ddammage <ddammmage@yandex.ru>
2022-09-08 12:38:11 +0300

    pppaaasd

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -2,6 +2,29 @@ import {MongoClient} from "mongodb"
 import {config} from "dotenv"
 config()
 
+
+
+const client = new MongoClient(process.env.MONGOURI || "mongodb://localhost:27017")
+
+// const client = new MongoClient ("mongodb://localhost:27017")
+
+
+
+export const db = client.db("it-incubator")
+export const bloggersCollection = db.collection<BloggerType>("bloggers")
+export const postsCollection = db.collection<PostsType>("posts")
+
+export async function runDb() {
+    try {
+        await client.connect();
+        await client.db("it-incubator").command({ ping: 1 });
+        console.log("Connected successfully to server");
+    } catch {
+        console.log('Connected failed')
+        await client.close();
+    }
+}
+
 export type BloggerType = {
     id: number
     name: string
@@ -20,24 +43,3 @@ export type PostsType = {
 }
 
 
-const client = new MongoClient(process.env.MONGOURI || "mongodb://localhost:27017")
-
-// const client = new MongoClient ("mongodb://localhost:27017")
-
-
-export const db = client.db("it-incubator")
-export const bloggersCollection = db.collection<BloggerType>("bloggers")
-export const postsCollection = db.collection<PostsType>("posts")
-
-
-
-export async function runDb() {
-    try {
-        await client.connect();
-        await client.db("it-incubator").command({ ping: 1 });
-        console.log("Connected successfully to server");
-    } catch {
-        console.log('Connected failed')
-        await client.close();
-    }
-}

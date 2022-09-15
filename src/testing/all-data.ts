877842f6594bf2df5024e9a86996f7d7c35ec2ba
ddammage <ddammmage@yandex.ru>
2022-09-15 13:19:55 +0300

    // return deleteAll in all-data.ts

--- src/testing/all-data.ts
+++ src/testing/all-data.ts
@@ -5,8 +5,8 @@ import {bloggersCollection} from "../repositories/db";
 export const testingRouter = Router({})
 
 testingRouter.delete('/all-data',  async (req: Request, res: Response) => {
-    const deleteAll = await bloggersCollection.deleteMany({})
-    return deleteAll
+    // const deleteAll = await bloggersCollection.deleteMany({})
+    // return deleteAll
     // напписать логику каую-то типа лезу в коллекцию  беру делит мени и  гг
     res.status(204).send()
 })
